7e54da1634ef61744fb6853e8b4a5e5fcdc49cbb
Giancarlo <giancarlo2709@gmail.com>
2020-09-12 09:26:09 -0500

    ambito de una variable

--- java-fundamentals/src/main/java/sesion02/EjemploMetodo.java
+++ java-fundamentals/src/main/java/sesion02/EjemploMetodo.java
@@ -9,6 +9,8 @@ Escriba un método que le pregunte a un usuario su nombre, luego lo saluda por s
 public class EjemploMetodo {
 
     private static Scanner scanner = new Scanner(System.in);
+    private static int numero1;
+    private static int numero2;
 
     public static void main(String[] args) {
         System.out.println("Ingresa tu nombre");
@@ -16,17 +18,27 @@ public class EjemploMetodo {
 
         System.out.println("Hola, " + obtenerNombre(nombre1));
 
-        int numero1 = 5;
-        int numero2 = 6;
+        System.out.println("Ingrese numero 1: ");
 
-        System.out.println("la suma es: " + calcularSuma(numero1, numero2));
+        numero1 = scanner.nextInt();//reserva un espacio en memoria, y guarda el valor en este espacio
+
+        System.out.println("Ingrese numero 2: ");
+
+        numero2 = scanner.nextInt();
+
+        System.out.println("la suma es: " + calcularSuma());
+        System.out.println("El producto es: " + calcularProducto(numero1, numero2));
     }
 
     public static String obtenerNombre(String nombre) {
         return nombre;
     }
 
-    public static int calcularSuma(int numero1, int numero2) {
+    public static int calcularSuma() {
         return numero1 + numero2;
+    }
+
+    public static int calcularProducto(int numero1, int numero2) {
+        return numero1 * numero2;
     }
 }
