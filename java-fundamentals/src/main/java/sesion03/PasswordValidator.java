72b9e1091a8d5325bdf99b089157b2ee3ad4e0c6
Giancarlo <giancarlo2709@gmail.com>
2020-09-19 09:57:54 -0500

    validador de password

---
+++ java-fundamentals/src/main/java/sesion03/PasswordValidator.java
@@ -0,0 +1,14 @@
+package sesion03;
+
+public class PasswordValidator {
+
+    private String username;
+    private String currentPassword;
+
+    private boolean isValid;
+    private String errorMessage;
+
+    public PasswordValidator(String username, String currentPassword) {
+
+    }
+}
