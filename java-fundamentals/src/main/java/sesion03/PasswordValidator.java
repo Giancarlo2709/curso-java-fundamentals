b696c90d3cd2e22cbf9d49e99810eac27346e72d
Giancarlo <giancarlo2709@gmail.com>
2020-09-19 10:05:10 -0500

    add login

--- java-fundamentals/src/main/java/sesion03/PasswordValidator.java
+++ java-fundamentals/src/main/java/sesion03/PasswordValidator.java
@@ -1,5 +1,7 @@
 package sesion03;
 
+import java.util.Scanner;
+
 public class PasswordValidator {
 
     private String username;
@@ -8,7 +10,43 @@ public class PasswordValidator {
     private boolean isValid;
     private String errorMessage;
 
-    public PasswordValidator(String username, String currentPassword) {
+    private static Scanner scanner = new Scanner(System.in);
 
+    public PasswordValidator(String username, String currentPassword) {
+        this.username = username;
+        this.currentPassword = currentPassword;
+    }
+
+    public boolean isValid() {
+        return isValid;
+    }
+
+    public String getErrorMessage() {
+        return errorMessage;
+    }
+
+    public static PasswordValidator login() {
+        System.out.println("Ingresa tu usuario");
+        String username = scanner.next();
+
+        System.out.println("Ingresa tu password");
+        String password = scanner.next();
+
+        return new PasswordValidator(username, password);
+    }
+
+    public static void main(String[] args) {
+        PasswordValidator validator = login();
+
+
+    }
+
+    public String getPasswordPropuesto() {
+        System.out.println("Ingresa tu nuevo password");
+        return scanner.next();
+    }
+
+    public void imprimirReglas() {
+        System.out.println("Tu nuevo password debe seguir los siguientes requerimientos");
     }
 }
