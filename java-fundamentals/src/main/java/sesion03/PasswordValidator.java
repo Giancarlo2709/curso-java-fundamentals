7d3a221c34098a01361510fcb57f2e76e1b662a9
Giancarlo <giancarlo2709@gmail.com>
2020-09-19 10:20:35 -0500

    validador de password

--- java-fundamentals/src/main/java/sesion03/PasswordValidator.java
+++ java-fundamentals/src/main/java/sesion03/PasswordValidator.java
@@ -37,7 +37,19 @@ public class PasswordValidator {
 
     public static void main(String[] args) {
         PasswordValidator validator = login();
+        validator.imprimirReglas();
 
+        do {
+            String passwordPropuesto = validator.getPasswordPropuesto();
+            validator.cambiarPassword(passwordPropuesto);
+
+            if(!validator.isValid()) {
+                System.out.println(validator.getErrorMessage());
+            }
+
+        } while(!validator.isValid());
+
+        System.out.println("El password ingresado es valido");
 
     }
 
@@ -48,5 +60,41 @@ public class PasswordValidator {
 
     public void imprimirReglas() {
         System.out.println("Tu nuevo password debe seguir los siguientes requerimientos");
+        System.out.println("* Al menos 8 caracteres");
+        System.out.println("* debe contener una letra mayuscula");
+        System.out.println("* debe contener una caracter especial");
+        System.out.println("* no debe contener el nombre de usuario");
+        System.out.println("* no debe ser el mismo password anterior");
+    }
+
+    public void cambiarPassword(String nuevoPassword) {
+        isValid = true;
+        errorMessage = "";
+
+        if (nuevoPassword.length() < 8) {
+            isValid = false;
+            errorMessage += "\n Tu password debe tener al menos 8 caracteres";
+        }
+
+        if (nuevoPassword.equals(nuevoPassword.toLowerCase())) {
+            isValid = false;
+            errorMessage += "\n Tu password debe incluir al menos una letra mayuscula";
+        }
+
+        if (nuevoPassword.matches("[a-zA-Z0-9]*")) {
+            isValid = false;
+            errorMessage += "\n Tu password debe incluir un caracter especial";
+        }
+
+        if (nuevoPassword.toUpperCase().contains(username.toUpperCase())) {
+            isValid = false;
+            errorMessage += "\n Tu password no debe contener el nombre del usuario";
+        }
+
+        if(nuevoPassword.equals(currentPassword)) {
+            isValid = false;
+            errorMessage += "\n Tu password debe ser diferente al actual";
+        }
+
     }
 }
