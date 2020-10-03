a617a6a61f0f713c345e23148766e66bef5a37e8
Giancarlo <giancarlo2709@gmail.com>
2020-10-03 10:48:33 -0500

    calculadora

--- java-fundamentals/src/main/java/reforzamiento/DoWhileExample.java
+++ java-fundamentals/src/main/java/reforzamiento/DoWhileExample.java
@@ -20,5 +20,76 @@ public class DoWhileExample {
 
     public static void main(String[] args) {
 
+        int opcion = 0;
+
+        do {
+            System.out.println("Seleccione una opción: ");
+            System.out.println("0. Salir");
+            System.out.println("1. Sumar");
+            System.out.println("2. Restar");
+            System.out.println("3. Multiplicar");
+            System.out.println("4. Dividir");
+
+            opcion = scanner.nextInt();
+
+            switch(opcion) {
+                case 0:
+                    System.out.println("Gracias por usar nuestro sistema de calculadora. Lo esperamos pronto");
+                    break;
+                case 1:
+                    sumar();
+                    break;
+                case 2:
+                    restar();
+                    break;
+                case 3:
+                    multiplicar();
+                    break;
+                case 4:
+                    dividir();
+                    break;
+                default:
+                    System.out.println("Ingrese una opcion válida");
+            }
+
+        } while (opcion != 0);
+
+    }
+
+    public static void sumar() {
+        System.out.println("Ingrese primer número");
+        int a = scanner.nextInt();
+        System.out.println("Ingrese segundo número");
+        int b = scanner.nextInt();
+        System.out.println("La suma es: " + (a + b));
+    }
+
+    public static void restar() {
+        System.out.println("Ingrese primer número");
+        int a = scanner.nextInt();
+        System.out.println("Ingrese segundo número");
+        int b = scanner.nextInt();
+        System.out.println("La diferencia es: " + (a - b));
+    }
+
+    public static void multiplicar() {
+        System.out.println("Ingrese primer número");
+        int a = scanner.nextInt();
+        System.out.println("Ingrese segundo número");
+        int b = scanner.nextInt();
+        System.out.println("El producto es: " + (a * b));
+    }
+
+    public static void dividir() {
+        System.out.println("Ingrese primer número");
+        int a = scanner.nextInt();
+        System.out.println("Ingrese segundo número");
+        int b = scanner.nextInt();
+
+        try {
+            System.out.println("La división es: " + (a / b));
+        } catch(Exception e) {
+            System.out.println("No se puede dividir por cero");
+        }
     }
 }
