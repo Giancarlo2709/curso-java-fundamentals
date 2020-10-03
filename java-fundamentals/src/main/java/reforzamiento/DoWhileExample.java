76699bdf460da6ca87f8156690e7c44382b3ec85
Giancarlo <giancarlo2709@gmail.com>
2020-10-03 09:53:52 -0500

    escribir calculadora

---
+++ java-fundamentals/src/main/java/reforzamiento/DoWhileExample.java
@@ -0,0 +1,19 @@
+package reforzamiento;
+/*
+ *Escribir una calculadora, donde yo pueda solicitar al usuario, que seleccione un menu, donde
+ * 0: salir
+ * 1: sumar
+ * 2: restar
+ * 3: multiplicar
+ * 4: dividir
+ * El programa debe solicitar la opcion a ejecutar (1, 2, 3, 4)
+ * luego según la acción debe solicitar el ingreso de dos numeros
+ * hacer el calculo de dichos numeros.
+ * el programa debe salir cuando elija la opcion 0
+ */
+public class DoWhileExample {
+
+    public static void main(String[] args) {
+
+    }
+}
